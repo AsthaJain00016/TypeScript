02a1e59ec3b264c810a72b29d6251bc89a5d29f4
AsthaJain00016 <jainastha587@gmail.com>
2026-03-18 17:52:12 +0530

    Union and Any

---
+++ Introduction/src/nionAndAny.ts
@@ -0,0 +1,8 @@
+// Union and Any
+let subs:number | string=10  // this is union
+
+let apiRequest: 'pending' | 'success' | 'error'='pending'
+
+// apiRequest="astha" // This is wrong
+
+// apiRequest='success' // This is correct
