f68b07afb7b0345893269202a27250f4a0dfb9ba
AsthaJain00016 <jainastha587@gmail.com>
2026-03-18 17:57:46 +0530

    delete

--- Introduction/src/nionAndAny.ts
+++
@@ -1,8 +0,0 @@
-// Union and Any
-let subs:number | string=10  // this is union
-
-let apiRequest: 'pending' | 'success' | 'error'='pending'
-
-// apiRequest="astha" // This is wrong
-
-// apiRequest='success' // This is correct
