0c4d274502eed088d537dbdded9db46118dbffe2
AsthaJain00016 <jainastha587@gmail.com>
2026-03-18 17:52:02 +0530

    Types in typesscript

---
+++ Introduction/src/typesInTS.ts
@@ -0,0 +1,15 @@
+let drink="Chai"
+
+let cups=Math.random()>0.5?10:5
+
+// Type Infrencing:- Type inference refers to the automatic detection of the type of an expression in a formal language, such as programming languages and mathematical type systems. This process allows the compiler to deduce the type of a variable or function without explicit type annotations, making programming more efficient and reducing the need for repetitive type declarations.
+
+let channelName="Astha"
+
+let name:string="Astha Jain" // Type annotation
+
+// Type Annotation:- TypeScript annotations allow developers to explicitly specify the types of variables, function parameters, return values, objects, and more. This ensures type safety, improves code readability, and helps catch errors during development.
+
+// let chaiOrder:number
+// let chaiOrder:boolean
+// let chaiOrder:string
